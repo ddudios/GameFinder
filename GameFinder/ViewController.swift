dc8edbd92630574b228573f97baea0bf84d565ed
ddudi <ddudios@naver.com>
2025-09-27 16:16:04 +0900

    Create Project
    Partially addresses #1: .gitignore

---
+++ GameFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameFinder
+//
+//  Created by Suji Jang on 9/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
