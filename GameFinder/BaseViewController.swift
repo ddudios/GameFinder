5f20de7f0a0a4af5c63ef28feed3a6658c50ad9b
ddudi <ddudios@naver.com>
2025-09-27 16:42:02 +0900

    feat: Project General Settings
    - iOS Deployment Target: iOS 16.0
    - Supported Destinations: only iPhone
    - Deployment Info: only Portrait
      - Supported Interface Orientations (iPad): only UIInterfaceOrientationPortrait
    - Appearance: Light
    - CodeBase: Remove Storyboard
    
    Partially addresses #1

---
+++ GameFinder/BaseViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BaseViewController.swift
+//  GameFinder
+//
+//  Created by Suji Jang on 9/27/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() { }
+    func configureLayout() { }
+    func configureView() {
+        view.backgroundColor = .systemBackground
+    }
+}
