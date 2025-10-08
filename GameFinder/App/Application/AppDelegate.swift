1189afbd70aa2cb4f49f2e03cbe0a0d08072cab8
ddudi <ddudios@naver.com>
2025-10-09 03:26:48 +0900

    feat: Firebase - Analytics & Crashlytics
    Partially addresses #6

--- GameFinder/App/Application/AppDelegate.swift
+++ GameFinder/App/Application/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import RealmSwift
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+        
+        FirebaseApp.configure()
         NavigationBar.configureAppearance()
 
         return true
