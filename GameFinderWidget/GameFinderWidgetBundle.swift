00609f3aa0ff3562a3c86f2bbf960392d4b321a6
ddudi <ddudios@naver.com>
2025-12-11 19:38:34 +0900

    Widget 추가

---
+++ GameFinderWidget/GameFinderWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  GameFinderWidgetBundle.swift
+//  GameFinderWidget
+//
+//  Created by Suji Jang on 12/11/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct GameFinderWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        GameFinderWidget()
+    }
+}
